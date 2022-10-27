a39e65949ed1223f1e8656f6775632928928985e
zhangchaosd <zhangchaogoo@gmail.com>
2022-10-27 15:56:24 +0800

    Update

--- FaceForensics++.py
+++ FaceForensics++.py
@@ -1,45 +1,41 @@
 import os
 
+import cv2
 from tqdm import tqdm
 
-from utils import *
+from utils import (
+    crop_img,
+    gen_dirs,
+    get_face_location,
+    get_files_from_path,
+    parse,
+    static_shuffle,
+    video2frames,
+)
 
-### TODO del
-def dfs(pa = '/Users/zhangchao/datasets/ff'):
-    if os.path.isdir(pa):
-        files = os.listdir(pa)
-        print(pa, len(files))
-        files = [os.path.join(pa, f) for f in files]
-        for f in files:
-            dfs(f)
-    else:
-        if pa.endswith('mp4'):
-            pass
-        else:
-            print('To remove', pa)
-            # os.remove(pa)
-
-pa = '/Users/zhangchao/datasets/ff'
-# dfs(pa)
-
-### TODO del
 
 def get_source_videos(videos):
     res = []
     for video in videos:
-        res.append(video[:3]+'.mp4')
+        res.append(video[:3] + '.mp4')
         res.append(video[4:])
     return res
+
 
 def double_videos(videos):
     res = []
     for video in videos:
-        res.append(video[4:7]+'_'+video[:3]+'.mp4')
+        res.append(video[4:7] + '_' + video[:3] + '.mp4')
     return res + videos
 
+
 def get_ff_splits(path):
-    videos = [video for video in get_files_from_path(path) if int(video[:3])<int(video[4:7])]
-    assert len(videos)==500, f'Missing some videos(expect 1000): {path}'
+    videos = [
+        video
+        for video in get_files_from_path(path)
+        if int(video[:3]) < int(video[4:7])
+    ]
+    assert len(videos) == 500, f'Missing some videos(expect 1000): {path}'
     static_shuffle(videos)
     train_split_m = videos[:360]
     val_split_m = videos[360:430]
@@ -53,24 +49,102 @@ def get_ff_splits(path):
     val_split_m = double_videos(val_split_m)
     test_split_m = double_videos(test_split_m)
 
-    return train_split_m + train_split_o, val_split_m + val_split_o, test_split_m + test_split_o
+    return (
+        train_split_m + train_split_o,
+        val_split_m + val_split_o,
+        test_split_m + test_split_o,
+    )
 
 
-def solve(dataset_path, faces_path, video_path, video_name, f, label, samples, face_scale, file_names = None, crop_datas = None):
-    frames_with_file_names = [*video2frames(os.path.join(dataset_path,video_path,video_name),samples)]
-    frames = [frame for _, frame in frames_with_file_names]
+def get_DFD_splits(original_path, manipulated_path):
+    train_list = [
+        '01',
+        '02',
+        '03',
+        '04',
+        '06',
+        '07',
+        '09',
+        '11',
+        '12',
+        '13',
+        '14',
+        '15',
+        '18',
+        '20',
+        '21',
+        '25',
+        '26',
+        '27',
+    ]
+    val_list = ['05', '08', '16', '17', '28']
+    test_list = ['10', '19', '22', '23', '24']
+    videos = get_files_from_path(original_path) + get_files_from_path(
+        manipulated_path
+    )
+    train_split = []
+    val_split = []
+    test_split = []
+    for video in videos:
+        if video[:2] in train_list:
+            train_split.append(video)
+        elif video[:2] in val_list:
+            val_split.append(video)
+        elif video[:2] in test_list:
+            test_split.append(video)
+        else:
+            assert False
+    return train_split, val_split, test_split
+
+
+def solve(
+    dataset_path,
+    faces_path,
+    video_path,
+    video_name,
+    f,
+    label,
+    samples,
+    face_scale,
+    file_names=None,
+    crop_datas=None,
+):
+    new_file_names, frames = video2frames(
+        os.path.join(dataset_path, video_path, video_name), samples
+    )
     if file_names is None:
-        file_names = [file_name for file_name, _ in frames_with_file_names]
-    file_names = [os.path.join(faces_path, video_path, file_name)for file_name, _ in frames_with_file_names]
+        file_names = new_file_names
+    file_names = [
+        os.path.join(faces_path, video_path, file_name)
+        for file_name in file_names
+    ]
     if crop_datas is None:
-        crop_datas = [*map(get_face_location, frames, [face_scale]*len(frames))]
-    # print(len(frames), len(file_names), len(crop_datas))
+        crop_datas = [
+            *map(get_face_location, frames, [face_scale] * len(frames))
+        ]
     assert len(frames) == len(file_names), f'{len(frames) != {len(file_names)}}'
-    assert len(crop_datas) == len(file_names), f'{len(crop_datas) != {len(file_names)}}'
+    assert len(crop_datas) == len(
+        file_names
+    ), f'{len(crop_datas) != {len(file_names)}}'
     faces = [*map(crop_img, frames, crop_datas)]
-    [*map(cv2.imwrite, file_names, faces, [[int(cv2.IMWRITE_JPEG_QUALITY),100]]*len(faces))]
+    [
+        *map(
+            cv2.imwrite,
+            file_names,
+            faces,
+            [[int(cv2.IMWRITE_JPEG_QUALITY), 100]] * len(faces),
+        )
+    ]
     if f is not None:
-        f.writelines([os.path.join('faces', video_path, os.path.basename(img_name))+' '+label+'\n' for img_name in file_names])
+        f.writelines(
+            [
+                os.path.join('faces', video_path, os.path.basename(img_name))
+                + ' '
+                + label
+                + '\n'
+                for img_name in file_names
+            ]
+        )
     return file_names, crop_datas
 
 
@@ -88,21 +162,42 @@ def main(path, samples, face_scale, subset):
     f_val_c40 = open(os.path.join(faces_path, 'val_c40.txt'), 'w')
     f_test_c40 = open(os.path.join(faces_path, 'test_c40.txt'), 'w')
 
-    ### Attention
-    train_split, val_split, test_split = get_ff_splits(os.path.join(path,'manipulated_sequences','Deepfakes','raw','videos'))
-
     # FaceShifter don't have masks
     if subset == 'FF':
-        datasets = ['original', 'Deepfakes', 'Face2Face', 'FaceSwap', 'NeuralTextures']
+        datasets = [
+            'original',
+            'Deepfakes',
+            'Face2Face',
+            'FaceSwap',
+            'NeuralTextures',
+        ]
+        train_split, val_split, test_split = get_ff_splits(
+            os.path.join(
+                path, 'manipulated_sequences', 'Deepfakes', 'raw', 'videos'
+            )
+        )
     else:
         datasets = ['DeepFakeDetection_original', 'DeepFakeDetection']
+        train_split, val_split, test_split = get_DFD_splits(
+            os.path.join(
+                path,
+                'manipulated_sequences',
+                'DeepFakeDetection',
+                'raw',
+                'videos',
+            )
+        )
     for i, dataset in enumerate(datasets):
         print(f'Now parsing {dataset}...')
         label = '0 '
         if 'original' == dataset:
-            raw_path = os.path.join('original_sequences', 'youtube', 'raw', 'videos')
+            raw_path = os.path.join(
+                'original_sequences', 'youtube', 'raw', 'videos'
+            )
         elif 'DeepFakeDetection_original' == dataset:
-            raw_path = os.path.join('original_sequences', 'actors', 'raw', 'videos')
+            raw_path = os.path.join(
+                'original_sequences', 'actors', 'raw', 'videos'
+            )
         else:
             raw_path = 'manipulated_sequences'
             raw_path = os.path.join(raw_path, dataset, 'raw', 'videos')
@@ -133,11 +228,53 @@ def main(path, samples, face_scale, subset):
                 f_c23 = f_test_c23
                 f_c40 = f_test_c40
 
-            file_names, crop_datas = solve(path, faces_path,raw_path,video,f_raw,label,samples,face_scale)
-            solve(path, faces_path,c23_path,video,f_c23,label,samples,face_scale,file_names,crop_datas)
-            solve(path, faces_path,c40_path,video,f_c40,label,samples,face_scale,file_names,crop_datas)
+            file_names, crop_datas = solve(
+                path,
+                faces_path,
+                raw_path,
+                video,
+                f_raw,
+                label,
+                samples,
+                face_scale,
+            )
+            solve(
+                path,
+                faces_path,
+                c23_path,
+                video,
+                f_c23,
+                label,
+                samples,
+                face_scale,
+                file_names,
+                crop_datas,
+            )
+            solve(
+                path,
+                faces_path,
+                c40_path,
+                video,
+                f_c40,
+                label,
+                samples,
+                face_scale,
+                file_names,
+                crop_datas,
+            )
             if 'original' not in dataset:
-                solve(path, faces_path,masks_path,video,None,None,samples,face_scale,file_names,crop_datas)
+                solve(
+                    path,
+                    faces_path,
+                    masks_path,
+                    video,
+                    None,
+                    None,
+                    samples,
+                    face_scale,
+                    file_names,
+                    crop_datas,
+                )
 
     f_train_raw.close()
     f_val_raw.close()
