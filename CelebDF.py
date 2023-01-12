d14f0737985aba6f87eb31102f3cafc36e651e9b
zhangchaosd <zhangchaogoo@gmail.com>
2023-01-12 16:30:03 +0800

    Update CelebDF

--- CelebDF.py
+++ CelebDF.py
@@ -1,105 +1,103 @@
+import multiprocessing as mp
 import os
+from functools import partial
 
 from tqdm import tqdm
 
-from utils import get_files_from_path, parse, parse_video, static_shuffle
+from utils import get_files_from_path, parse, gen_dirs, video2face_pngs
 
 
-def main(path, samples, face_scale):
-    faces_path = os.path.join(path, 'faces')
-    if not os.path.exists(faces_path):
-        os.mkdir(faces_path)
-    train_info_txt = os.path.join(faces_path, 'train_info.txt')
-    val_info_txt = os.path.join(faces_path, 'val_info.txt')
-    test_info_txt = os.path.join(faces_path, 'test_info.txt')
+def f2(line, path, faces_prefix, samples, face_scale, detector):
+    label, video_path = line.split(' ')
+    rela_path = os.path.dirname(video_path)
+    save_path = os.path.join(path, faces_prefix, rela_path)
+    video_path = os.path.join(path, video_path)
+    infos = [
+        os.path.join(faces_prefix, rela_path, img_name) + f'\t{label}\n'
+        for img_name in video2face_pngs(
+            video_path, save_path, samples, face_scale, detector
+        )
+    ]
+    return infos
+
+def f1(mode, lines, path, faces_prefix, samples, face_scale, detector, num_workers):
+    print(f'Parsing {mode}...')
+    txt_path = os.path.join(path, faces_prefix, mode + '.txt')
+    infos = []
+    with mp.Pool(num_workers) as workers, open(txt_path, 'w') as f:
+        with tqdm(total=len(lines)) as pbar:
+            for info in workers.imap_unordered(
+                partial(
+                    f2,
+                    path=path,
+                    faces_prefix=faces_prefix,
+                    samples=samples,
+                    face_scale=face_scale,
+                    detector=detector,
+                ),
+                lines,
+            ):
+                pbar.update()
+                infos += info
+        f.writelines(infos)
+    print(mode, len(infos))
+    return infos
+
+def main(path, samples, face_scale, detector, num_workers):
+    faces_prefix = 'faces' + str(samples) + detector
+    gen_dirs(os.path.join(path, faces_prefix))
 
     test_txt = os.path.join(path, 'List_of_testing_videos.txt')
     with open(test_txt, 'r') as f:
         test_lines = f.read().splitlines()
     test_videos = []
-    with open(test_info_txt, 'w') as f:
-        print('Generating test split...')
-        for l in tqdm(test_lines):
-            label, video = l.split(' ')
-            test_videos.append(video)
-            parse_video(
-                path,
-                os.path.dirname(video),
-                os.path.basename(video),
-                label,
-                f,
-                samples,
-                face_scale,
-            )
 
-    celeb_real_path = os.path.join(path, 'Celeb-real')
-    celeb_synthesis_path = os.path.join(path, 'Celeb-synthesis')
-    youtube_real_path = os.path.join(path, 'YouTube-real')
-    celeb_real_videos = [
+    for l in test_lines:
+        _, video = l.split(' ')
+        test_videos.append(video)
+    assert len(test_videos) == 100 or len(test_videos) == 518
+
+    real_videos = [
         os.path.join('Celeb-real', video)
-        for video in get_files_from_path(celeb_real_path)
+        for video in get_files_from_path(os.path.join(path, 'Celeb-real'))
     ]
-    celeb_synthesis_videos = [
-        os.path.join('Celeb-synthesis', video)
-        for video in get_files_from_path(celeb_synthesis_path)
-    ]
-    youtube_real_videos = [
+    real_videos += [
         os.path.join('YouTube-real', video)
-        for video in get_files_from_path(youtube_real_path)
+        for video in get_files_from_path(os.path.join(path, 'YouTube-real'))
+    ]
+    synthesis_videos = [
+        os.path.join('Celeb-synthesis', video)
+        for video in get_files_from_path(os.path.join(path, 'Celeb-synthesis'))
     ]
 
-    celeb_real_videos = [
-        *filter(lambda v: v not in test_videos, celeb_real_videos)
+    real_videos = [
+        *filter(lambda v: v not in test_videos, real_videos)
     ]
-    celeb_synthesis_videos = [
-        *filter(lambda v: v not in test_videos, celeb_synthesis_videos)
-    ]
-    youtube_real_videos = [
-        *filter(lambda v: v not in test_videos, youtube_real_videos)
+    synthesis_videos = [
+        *filter(lambda v: v not in test_videos, synthesis_videos)
     ]
 
-    all_videos = [('0', v) for v in celeb_real_videos + youtube_real_videos] + [
-        ('1', v) for v in celeb_synthesis_videos
+    train_lines = [('0 ' + v) for v in real_videos] + [
+        ('1 ' + v) for v in synthesis_videos
     ]
+    assert len(train_lines) == 1103 or len(train_lines) == 6011
 
-    static_shuffle(all_videos)
-    val_size = 0  #
-    train_split = all_videos[: len(all_videos) - val_size]
-    with open(train_info_txt, 'w') as f:
-        print('Generating train split...')
-        for l in tqdm(train_split):
-            label, video = l
-            parse_video(
-                path,
-                os.path.dirname(video),
-                os.path.basename(video),
-                label,
-                f,
-                samples,
-                face_scale,
-            )
-    val_split = all_videos[len(all_videos) - val_size :]
-    with open(val_info_txt, 'w') as f:
-        print('Generating val split...')
-        for l in tqdm(val_split):
-            label, video = l
-            parse_video(
-                path,
-                os.path.dirname(video),
-                os.path.basename(video),
-                label,
-                f,
-                samples,
-                face_scale,
-            )
+    all_info = f1('train', train_lines, path, faces_prefix, samples, face_scale, detector, num_workers)
+    all_info += f1('test', test_lines, path, faces_prefix, samples, face_scale, detector, num_workers)
+    with open(os.path.join(path, faces_prefix, 'all.txt'), 'w') as f:
+        f.writelines(all_info)
+    print('All ', len(all_info))
+
 
 
 # real is 0
+# python CelebDF.py -path '/share/home/zhangchao/datasets_io03_ssd/Celeb-DF' -samples 120 -scale 1.3 -detector dlib -workers 8
+# python CelebDF.py -path '/share/home/zhangchao/datasets_io03_ssd/Celeb-DF-v2' -samples 120 -scale 1.3 -detector dlib -workers 8
 if __name__ == '__main__':
-    # args = parse()
-    # main(args.path, args.samples, args.scale)
-    print('v1')
-    main('/share/home/zhangchao/datasets_io03_ssd/Celeb-DF', 100, 1.3)
-    print('v2')
-    main('/share/home/zhangchao/datasets_io03_ssd/Celeb-DF-v2', 100, 1.3)
-    print('done')
+    args = parse()
+    main(args.path, args.samples, args.scale, args.detector, args.workers)
+    # print('v1')
+    # main('/share/home/zhangchao/datasets_io03_ssd/Celeb-DF', 100, 1.3)
+    # print('v2')
+    # main('/share/home/zhangchao/datasets_io03_ssd/Celeb-DF-v2', 100, 1.3)
+    # print('done')
