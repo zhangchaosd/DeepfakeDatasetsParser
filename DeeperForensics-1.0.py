028c8dea5ebe8d91fbcd40efd52fd2adc06b077b
zhangchaosd <zhangchaogoo@gmail.com>
2023-03-10 12:44:59 +0800

    Update

--- DeeperForensics-1.0.py
+++ DeeperForensics-1.0.py
@@ -1,8 +1,6 @@
 import os
 
-from utils import gen_dirs, parse, read_txt, parse_videos_mp, video2face_pngs
-
-modes = ['train', 'val', 'test']
+from utils import gen_dirs, parse, parse_videos_mp, read_txt, video2face_pngs
 
 
 def parse_videos(videos, get_key):
@@ -16,13 +14,13 @@ def parse_videos(videos, get_key):
     return d
 
 
-def parse_one_video(video, path, label, faces_prefix, samples, face_scale, detector):
+def parse_one_video(video, path, save_path, label, faces_prefix, samples, face_scale, detector):
     rela_path = os.path.dirname(video)
-    save_path = os.path.join(path, faces_prefix, rela_path)
+    save_path = os.path.join(save_path, faces_prefix, rela_path)
     video_path = os.path.join(path, video)
     gen_dirs(os.path.join(path, faces_prefix, rela_path))
     infos = [
-        os.path.join(faces_prefix, rela_path, img_name) + f'\t{label}\n'
+        os.path.join(rela_path, img_name) + f'\t{label}\n'
         for img_name in video2face_pngs(
             video_path, save_path, samples, face_scale, detector
         )
@@ -30,9 +28,10 @@ def parse_one_video(video, path, label, faces_prefix, samples, face_scale, detec
     return infos
 
 
-def main(path, samples, face_scale, detector, num_workers):
+def main(path, save_path, samples, face_scale, detector, num_workers, part):
+    assert part>=0 and part<7
     faces_prefix = f'faces{samples}{detector}'
-    gen_dirs(os.path.join(path, faces_prefix))
+    gen_dirs(os.path.join(save_path, faces_prefix))
 
     source_videos = read_txt(
         os.path.join(
@@ -51,6 +50,7 @@ def main(path, samples, face_scale, detector, num_workers):
         )
     )
     manipulated_videos = parse_videos(manipulated_videos, lambda s:os.path.basename(s)[4:8])
+    modes = ['train', 'val', 'test']
     all_infos = []
     for mode in modes:
         print(f'Parsing {mode} split...')
@@ -62,20 +62,28 @@ def main(path, samples, face_scale, detector, num_workers):
         for info in split_infos:
             s_videos += source_videos[info[4:8]]
             m_videos += manipulated_videos[info[4:8]]
-        infos = parse_videos_mp(s_videos,'0',path,faces_prefix, samples, face_scale, detector, num_workers, f'Parsing {mode} source...', parse_one_video)
-        infos += parse_videos_mp(m_videos,'1',path,faces_prefix, samples, face_scale, detector, num_workers, f'Parsing {mode} manipulated...', parse_one_video)
-        with open(os.path.join(path, faces_prefix, f'{mode}.txt')) as f:
+        length = len(s_videos)
+        unit = length // 6
+        s_videos=s_videos[part*unit:min(part*unit+unit,length)]
+        length = len(m_videos)
+        unit = length // 6
+        m_videos=m_videos[part*unit:min(part*unit+unit,length)]
+        print(f's: {len(s_videos)}')
+        print(f'm: {len(m_videos)}')
+        infos = parse_videos_mp(s_videos,'0',path,save_path,faces_prefix, samples, face_scale, detector, num_workers, f'Parsing {mode} source...', parse_one_video)
+        infos += parse_videos_mp(m_videos,'1',path,save_path,faces_prefix, samples, face_scale, detector, num_workers, f'Parsing {mode} manipulated...', parse_one_video)
+        with open(os.path.join(save_path, faces_prefix, f'{mode}_{part}.txt'), "w") as f:
             f.writelines(infos)
         all_infos += infos
-    with open(os.path.join(path, faces_prefix, 'all.txt')) as f:
-        f.writelines(infos)
+    with open(os.path.join(save_path, faces_prefix, f'all_{part}.txt'), "w") as f:
+        f.writelines(all_infos)
 
 
 # 1 is fake
-# python DeeperForensics-1.0.py -path '/share/home/zhangchao/datasets_io03_ssd/DeeperForensics-1.0' -samples 120 -scale 1.3 -detector dlib -workers 8
+# python DeeperForensics-1.0.py -path '/share/home/zhangchao/datasets_io03_ssd/DeeperForensics-1.0' -save_path '/share/home/zhangchao/datasets_io03_ssd/DeeperForensics-1.02' -samples 20 -scale 1.3 -detector dlib -workers 32 -part 0
 if __name__ == '__main__':
     args = parse()
-    main(args.path, args.samples, args.scale, args.detector, args.workers)
+    main(args.path, args.save_path, args.samples, args.scale, args.detector, args.workers, args.part)
 
 # 703,96,201   980_W010.mp4
 # source 48475+189 source_videos/W136/light_up/surprise/camera_down/W136_light_up_surprise_camera_down.mp4
