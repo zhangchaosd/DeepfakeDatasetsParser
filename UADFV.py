f152ae1ce5ea568b0728be13d9d217bdca93461b
zhangchaosd <zhangchaogoo@gmail.com>
2023-06-24 10:03:07 +0800

    Update UADFV

--- UADFV.py
+++ UADFV.py
@@ -1,134 +1,60 @@
-import multiprocessing as mp
+import concurrent.futures
+import json
 import os
-from functools import partial
+import sys
+from glob import glob
 
 from tqdm import tqdm
 
-from utils import (
-    gen_dirs,
-    get_files_from_path,
-    parse,
-    static_shuffle,
-    video2face_pngs,
-)
+from utils2 import parse_video
 
 
-def get_all_videos(real_videos):
-    fake_videos = []
-    for v in real_videos:
-        fake_videos.append(v[:-4].replace('real', 'fake') + '_fake.mp4')
-    return fake_videos + real_videos
+def handle_video(args):
+    dataset_path, video_path, num_frames_to_extract, label = args
+    return parse_video(dataset_path, video_path, num_frames_to_extract, label)
 
 
-def get_splits(videos):
-    static_shuffle(videos)
-    return (
-        get_all_videos(videos[:30]),
-        get_all_videos(videos[30:39]),
-        get_all_videos(videos[39:]),
-    )  # 30*2, 9*2, 10*2
-
-
-def parse_video(video_path, path, faces_path, samples, face_scale, detector):
-    if video_path[-9] == '_':
-        k = 'fake'
-        label = '1'
-    else:
-        k = 'real'
-        label = '0'
-    video_path = os.path.join(path, video_path)
-    save_path = os.path.join(faces_path, k)
-    infos = [
-        os.path.join(k, img_name) + '\t' + label + '\n'
-        for img_name in video2face_pngs(
-            video_path, save_path, samples, face_scale, detector
-        )
+def main(dataset_path, num_frames_to_extract):
+    all_infos = []
+    all_landmarks = {}
+    video_paths = [
+        (os.path.relpath(video_path, dataset_path), 0)
+        for video_path in glob(os.path.join(dataset_path, "real/*.mp4"))
+    ] + [
+        (os.path.relpath(video_path, dataset_path), 1)
+        for video_path in glob(os.path.join(dataset_path, "fake/*.mp4"))
     ]
-    return infos
-
-
-def parse_split(
-    path, faces_path, split, mode, samples, face_scale, detector, num_workers
-):
-    txt_path = os.path.join(faces_path, mode + '.txt')
-    infos = []
-    with mp.Pool(num_workers) as workers, open(txt_path, 'w') as f:
-        with tqdm(total=len(split)) as pbar:
-            for info in workers.imap_unordered(
-                partial(
-                    parse_video,
-                    path=path,
-                    faces_path=faces_path,
-                    samples=samples,
-                    face_scale=face_scale,
-                    detector=detector,
-                ),
-                split,
-            ):
-                pbar.update()
-                infos += info
-        f.writelines(infos)
-    return infos
-
-
-def main(path, save_path, samples, face_scale, detector, num_workers):
-    faces_prefix = 'faces' + str(samples) + detector
-    faces_path = os.path.join(save_path, faces_prefix)
-    gen_dirs(os.path.join(save_path, faces_prefix))
-    fake_rela_path = 'fake'
-    real_rela_path = 'real'
-    fake_videos = get_files_from_path(os.path.join(path, fake_rela_path))
-    real_videos = get_files_from_path(os.path.join(path, real_rela_path))
-    assert len(fake_videos) == 49
-    assert len(real_videos) == 49
-    real_videos = [
-        os.path.join(path, real_rela_path, video) for video in real_videos
-    ]
-    train_split, val_split, test_split = get_splits(real_videos)
-
-    train_info = parse_split(
-        path,
-        faces_path,
-        train_split,
-        'train',
-        samples,
-        face_scale,
-        detector,
-        num_workers,
+    assert len(video_paths) == 49 * 2
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for infos, landmarks in tqdm(
+            executor.map(
+                handle_video,
+                [
+                    (dataset_path, video_path, num_frames_to_extract, label)
+                    for video_path, label in video_paths
+                ],
+            ),
+            total=len(video_paths),
+        ):
+            all_infos.extend(infos)
+            all_landmarks.update(landmarks)
+    all_txt_path = os.path.join(
+        dataset_path, f"faces_{num_frames_to_extract}", "all.txt"
     )
-    val_info = parse_split(
-        path,
-        faces_path,
-        val_split,
-        'val',
-        samples,
-        face_scale,
-        detector,
-        num_workers,
-    )
-    test_info = parse_split(
-        path,
-        faces_path,
-        test_split,
-        'test',
-        samples,
-        face_scale,
-        detector,
-        num_workers,
-    )
-
-    all_info = train_info + val_info + test_info
-    all_txt = os.path.join(faces_path, 'all.txt')
-    with open(all_txt, 'w') as f:
-        f.writelines(all_info)
-    print('Num of train: ', len(train_info))
-    print('Num of val: ', len(val_info))
-    print('Num of test: ', len(test_info))
-    print('Total: ', len(all_info))
+    with open(all_txt_path, "w") as f:
+        f.writelines(all_infos)
+    # print("Num of train: ", len(train_info))
+    # print("Num of val: ", len(val_info))
+    # print("Num of test: ", len(test_info))
+    print("Total: ", len(all_infos))
+    landmarks_path = all_txt_path[:-3] + "js"
+    with open(landmarks_path, "w") as f:
+        json.dump(all_landmarks, f)
 
 
 # 0 is real
-# python UADFV.py -path '/share/home/zhangchao/datasets_io03_ssd/UADFV' -samples 50 -scale 1.3 -detector dlib -workers 8
-if __name__ == '__main__':
-    args = parse()
-    main(args.path, args.save_path, args.samples, args.scale, args.detector, args.workers)
+# python UADFV2.py '/share/home/zhangchao/datasets_io03_ssd/UADFV' 3
+if __name__ == "__main__":
+    dataset_path = sys.argv[1]
+    num_frames_to_extract = int(sys.argv[2])
+    main(dataset_path, num_frames_to_extract)
